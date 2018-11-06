f4d866e8aea944401e17bf44a88a2caa63464af4
Albina Zakirova <zak1804@gmail.com>
2018-11-06 03:02:44 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,10 +1,3 @@
 def current_age_for_birth_year(birth_year)
   2003-birth_ye
-  puts "What year were you born?"
-  birth_year = gets.to_i
-  
-  users_age = current_age_for_birth_year(birth_year)
-  
-  puts 'You are: ' + users_age.to_s + "
-yaers old.  "
 end
